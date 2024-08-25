4e204d1480e822bdc9c342b729e8f721003ca3b0
Jared Taylor <jaredtaylor.99@gmail.com>
2024-08-26 08:07:48 +1200

    Actor comp for future use

---
+++ Source/Tether/Private/TetherActorComponent.cpp
@@ -0,0 +1,8 @@
+﻿// Copyright (c) 2024 Jared Taylor. All Rights Reserved.
+
+
+#include "TetherActorComponent.h"
+
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(TetherActorComponent)
+
---
+++ Source/Tether/Public/TetherActorComponent.h
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2024 Jared Taylor. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "TetherActorComponent.generated.h"
+
+
+/**
+ * Handles tether physics in an actor component
+ */
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class TETHER_API UTetherActorComponent : public UActorComponent
+{
+	GENERATED_BODY()
+};
