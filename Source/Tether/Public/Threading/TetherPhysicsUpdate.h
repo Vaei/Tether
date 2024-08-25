8a516545af32fa51b52bb704d70eb90bfe80ff23
Jared Taylor <jaredtaylor.99@gmail.com>
2024-08-26 11:17:59 +1200

    Implement struct to assist physics subticking

---
+++ Source/Tether/Public/Threading/TetherPhysicsUpdate.h
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Jared Taylor. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+struct FTetherPhysicsUpdate
+{
+	float DeltaTime;
+	float TimeTick;
+	double RemainingTime;
+
+	FTetherPhysicsUpdate(float SimulationFrameRate)
+		: DeltaTime(0.0f),
+		  TimeTick(1.0f / SimulationFrameRate),
+		  RemainingTime(0.0f)
+	{
+	}
+
+	void StartFrame(float DeltaTime)
+	{
+		// Accumulate the time since the last frame
+		RemainingTime += DeltaTime;
+	}
+
+	bool ShouldTick() const
+	{
+		return RemainingTime >= TimeTick;
+	}
+
+	void FinalizeTick()
+	{
+		// Adjust the remaining time for the next sub-tick
+		RemainingTime -= TimeTick;
+	}
+};
