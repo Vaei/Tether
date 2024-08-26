a00ce747d808b2edfe7ae5a8de3bc7845beb147e
Jared Taylor <jaredtaylor.99@gmail.com>
2024-08-26 18:11:36 +1200

    Add incomplete gameplay task

--- Source/Tether/Private/Threading/TetherPhysicsTask.cpp
+++ Source/Tether/Private/Threading/TetherPhysicsTask.cpp
@@ -3,4 +3,45 @@
 
 #include "Threading/TetherPhysicsTask.h"
 
+#include "TetherActorComponent.h"
+
 #include UE_INLINE_GENERATED_CPP_BY_NAME(TetherPhysicsTask)
+
+UTetherPhysicsTask::UTetherPhysicsTask(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+	, LinearSolverTag(FGameplayTag::EmptyTag)
+	, AngularSolverTag(FGameplayTag::EmptyTag)
+{
+	bTickingTask = true;
+}
+
+UTetherPhysicsTask* UTetherPhysicsTask::TetherPhysicsTask(UTetherActorComponent* InTaskOwner, FGameplayTag LinearSolver, FGameplayTag AngularSolver, uint8 TaskPriority)
+{
+	if (!IsValid(InTaskOwner))
+	{
+		return nullptr;
+	}
+
+	check(InTaskOwner->Implements<UGameplayTaskOwnerInterface>());
+
+	if (UTetherPhysicsTask* Task = NewObject<UTetherPhysicsTask>(GetTransientPackage(), StaticClass()))
+	{
+		IGameplayTaskOwnerInterface* OwnerInterface = CastChecked<IGameplayTaskOwnerInterface>(InTaskOwner);
+		Task->InitTask(*OwnerInterface, TaskPriority);
+		
+		Task->LinearSolverTag = LinearSolver;
+		Task->AngularSolverTag = AngularSolver;
+		
+		return Task;
+	}
+
+	return nullptr;
+}
+
+void UTetherPhysicsTask::SetLinearSolver(FGameplayTag LinearSolver)
+{
+}
+
+void UTetherPhysicsTask::SetAngularSolver(FGameplayTag AngularSolver)
+{
+}
--- Source/Tether/Public/Threading/TetherPhysicsTask.h
+++ Source/Tether/Public/Threading/TetherPhysicsTask.h
@@ -8,6 +8,9 @@
 
 #include "TetherPhysicsTask.generated.h"
 
+class UTetherActorComponent;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FTetherPhysicsTaskUpdate, float, DeltaTime, bool, bTemp);
+
 /**
  * @TODO
  * Updates on GameThread
@@ -18,7 +21,33 @@ class TETHER_API UTetherPhysicsTask : public UGameplayTask
 	GENERATED_BODY()
 
 public:
-	// UTetherPhysicsTask(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+	UTetherPhysicsTask(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
 
-	// static UTetherPhysicsTask* TetherPhysicsTask(FGameplayTag LinearSolver);
+	/**
+	 * @param TaskPriority Controls how this task will be treated in relation to other, already running tasks, provided GameplayTasksComponent is configured to care about priorities (the default behavior)
+	 */
+	UFUNCTION(BlueprintCallable, Category=Tether, meta=(DefaultToSelf="InTaskOwner", BlueprintInternalUseOnly="TRUE", DisplayName="Tether Physics Update"))
+	static UTetherPhysicsTask* TetherPhysicsTask(UTetherActorComponent* TaskOwner, FGameplayTag LinearSolver, FGameplayTag AngularSolver, uint8 TaskPriority = 200);
+
+	UFUNCTION(BlueprintCallable, Category=Tether)
+	void SetLinearSolver(FGameplayTag LinearSolver);
+
+	UFUNCTION(BlueprintCallable, Category=Tether)
+	void SetAngularSolver(FGameplayTag AngularSolver);
+
+	UFUNCTION(BlueprintCallable, Category=Tether)
+	void PausePhysics() { Pause(); }
+
+	UFUNCTION(BlueprintCallable, Category=Tether)
+	void ResumePhysics() { Resume(); }
+
+	// virtual void Activate() override;
+	
+protected:
+	FGameplayTag LinearSolverTag;
+	FGameplayTag AngularSolverTag;
+
+protected:
+	UPROPERTY(BlueprintAssignable)
+	FTetherPhysicsTaskUpdate PhysicsUpdate;
 };
