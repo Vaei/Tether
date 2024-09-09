6277cfafbad79483ba1f395d5b20f2e777769ff9
Jared Taylor <jaredtaylor.99@gmail.com>
2024-09-09 14:34:25 +1200

    Add enums for sim mode and wake state

---
+++ Source/TetherPhysics/Public/TetherPhysicsTypes.h
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Jared Taylor. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TetherPhysicsTypes.generated.h"
+
+/**
+ * Defines how a physics object is controlled within the Tether physics engine.
+ */
+UENUM(BlueprintType)
+enum class ETetherSimulationMode : uint8
+{
+	Simulated			UMETA(ToolTip="Simulated by the physics engine"),
+	Kinematic			UMETA(ToolTip="Does not respond to physics forces"),
+};
+
+/**
+ * Defines the wake/sleep state of a physics object, determining whether it's actively simulated or idle.
+ */
+UENUM(BlueprintType)
+enum class ETetherWakeState : uint8
+{
+	Awake				UMETA(ToolTip="Awake and actively simulated"),
+	ForceAwake			UMETA(ToolTip="Awake and actively simulated - will not sleep"),
+	Asleep				UMETA(ToolTip="Asleep and not simulated until disturbed"),
+	ForceAsleep			UMETA(ToolTip="Asleep and not simulated even if disturbed - will not wake"),
+};
